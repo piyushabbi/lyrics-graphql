36de7d37e47e6f43fe2d0dce43be441dddd39d45
piyushabbi <piyushabbi27@gmail.com>
2018-06-19 22:42:10 +0530

    Query Graphql to fetch song list

--- client/components/SongList/SongList.js
+++ client/components/SongList/SongList.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
 	render() {
-		return <div>SongList</div>;
+		const songItems = this.props.data.songs
+			? this.props.data.songs.map(m => (
+					<li key={m.id} className="collection-item">
+						{m.title}
+					</li>
+			  ))
+			: null;
+		if (this.props.data.loading) {
+			return <i>Loading...</i>;
+		}
+		return (
+			<section className="container">
+				<h1>SongList</h1>
+				<ul className="collection">{songItems}</ul>
+			</section>
+		);
 	}
 }
 
-export default SongList;
+const query = gql`
+	{
+		songs {
+			title
+			id
+		}
+	}
+`;
+
+export default graphql(query)(SongList);
