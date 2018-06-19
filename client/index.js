8610a2366295aecb02bf7ec0b3092780a57555b4
piyushabbi <piyushabbi27@gmail.com>
2018-06-19 22:00:11 +0530

    Apollo Boost and First Component

--- client/index.js
+++ client/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+// Components
+import SongList from './components/SongList/SongList';
+
+const client = new ApolloClient({
+	uri: 'http://localhost:4000/graphql'
+});
 
 const Root = () => {
-  return <div>Lyrical</div>
+	return (
+		<ApolloProvider client={client}>
+			<SongList />
+		</ApolloProvider>
+	);
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector('#root'));
