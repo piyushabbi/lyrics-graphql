d55acfeeb3dd0b68267faae521c66ab2faa403e1
piyushabbi <piyushabbi27@gmail.com>
2018-06-20 00:07:53 +0530

    SourceMaps | Add Song Form

--- client/components/AddSong/AddSong.js
+++ client/components/AddSong/AddSong.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-const AddSong = () => {
-	return <div>Add Song</div>;
-};
+class AddSong extends Component {
+	constructor() {
+		super();
+		this.state = {
+			songValue: ''
+		};
+		this.onSubmitHandler = this.onSubmitHandler.bind(this);
+	}
 
-export default AddSong;
+	onSubmitHandler(e) {
+		e.preventDefault();
+		this.props.mutate({
+			variables: {
+				title: this.state.songValue
+			}
+		});
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<h1>Create a Song</h1>
+				<form onSubmit={this.onSubmitHandler}>
+					<label>Song Title</label>
+					<input
+						type="text"
+						value={this.state.songValue}
+						onChange={e => this.setState({ songValue: e.target.value })}
+					/>
+				</form>
+			</div>
+		);
+	}
+}
+
+const mutation = gql`
+	mutation AddSong($title: String) {
+		addSong(title: $title) {
+			title
+		}
+	}
+`;
+
+export default graphql(mutation)(AddSong);
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+	devtool: 'eval-source-map',
 	mode: 'development',
 	entry: './client/index.js',
 	output: {
