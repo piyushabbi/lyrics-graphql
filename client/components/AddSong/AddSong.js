c46d3e7c6fee969819632510b22653ea667350b3
piyushabbi <piyushabbi27@gmail.com>
2018-06-21 21:30:00 +0530

    Navigation on successfull mutation

--- client/components/AddSong/AddSong.js
+++ client/components/AddSong/AddSong.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router-dom';
 
 class AddSong extends Component {
 	constructor() {
@@ -13,16 +14,19 @@ class AddSong extends Component {
 
 	onSubmitHandler(e) {
 		e.preventDefault();
-		this.props.mutate({
-			variables: {
-				title: this.state.songValue
-			}
-		});
+		this.props
+			.mutate({
+				variables: {
+					title: this.state.songValue
+				}
+			})
+			.then(() => this.props.history.push('/'));
 	}
 
 	render() {
 		return (
 			<div className="container">
+				<Link to="/">Back</Link>
 				<h1>Create a Song</h1>
 				<form onSubmit={this.onSubmitHandler}>
 					<label>Song Title</label>
--- client/components/SongList/SongList.js
+++ client/components/SongList/SongList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router-dom';
 
 class SongList extends Component {
 	render() {
@@ -22,6 +23,9 @@ class SongList extends Component {
 			<section className="container">
 				<h1>SongList</h1>
 				<ul className="collection">{songItems}</ul>
+				<Link to="/add-song" className="btn-floating btn-large red right">
+					<i className="material-icons">add</i>
+				</Link>
 			</section>
 		);
 	}
