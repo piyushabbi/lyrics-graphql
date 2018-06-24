d55495d544341c5abc38afbe34fefca4e8df853c
piyushabbi <piyushabbi27@gmail.com>
2018-06-24 16:58:58 +0530

    Like Lyric Mutation and like component

--- client/components/LyricList/LyricList.js
+++ client/components/LyricList/LyricList.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
+	likeLyric(id) {
+		this.props.mutate({
+			variables: {
+				id
+			}
+		});
+	}
 	render() {
 		const list =
 			this.props.lyrics &&
 			this.props.lyrics.map(lyric => (
 				<li key={lyric.id} className="collection-item">
 					{lyric.content}
+					<i className="right">({lyric.likes})</i>
+					<i
+						style={{ color: 'royalblue', cursor: 'pointer' }}
+						className="material-icons right"
+						onClick={() => this.likeLyric(lyric.id)}
+					>
+						thumb_up
+					</i>
 				</li>
 			));
 		return (
@@ -22,4 +39,13 @@ class LyricList extends Component {
 	}
 }
 
-export default LyricList;
+const mutation = gql`
+	mutation LikeLyric($id: ID) {
+		likeLyric(id: $id) {
+			id
+			likes
+		}
+	}
+`;
+
+export default graphql(mutation)(LyricList);
